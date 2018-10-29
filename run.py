097b460afe816c797b71fdf0375e80ebb0fe2c33
Unknown <tklubal@gmail.com>
2018-10-29 16:39:24 +0100

    Wireshark
    No wireshark
    Pcap added
    Frame depacker

---
+++ run.py
@@ -0,0 +1,109 @@
+import sys
+import os
+import traceback
+import time
+import datetime
+import pcapy
+import dpkt
+from subtypes import *
+import json
+
+
+interface = 'wlo1'
+monitor_enable  = ''
+monitor_disable = ''
+change_channel  = 'iw dev wlo1 set channel %s'
+
+channels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+
+def start():
+    #os.system(monitor_enable)
+    try: sniff(interface)
+    except KeyboardInterrupt: 
+        with open("data_file.json", "w") as write_file:
+            json.dump(recordToExport, write_file)
+        print foudedMac    
+        print(str(len(foudedMac)) + " Found")
+        sys.exit()
+
+
+def to_address(address):
+    return ':'.join('%02x' % ord(b) for b in address)
+
+def sniff(interface):
+    channels_switch(5)
+    max_packet_size = 256
+    promiscuous = 0
+    timeout = 100
+    packets = pcapy.open_live(interface, max_packet_size, promiscuous, timeout)
+    packets.setfilter('') 
+    
+    def loop(header, data):
+        
+        try:
+            packet = dpkt.radiotap.Radiotap(data)
+            packet_signal = -(256 - packet.ant_sig.db)
+            frame = packet.data
+            
+            if frame.type == dpkt.ieee80211.MGMT_TYPE:
+                record = {
+                    'timestamp': '0',
+                    'type': 'management',
+                    'subtype': subtypes_management[frame.subtype],
+                    'strength': packet_signal,
+                    'source_address': to_address(frame.mgmt.src),
+                    'destination_address': to_address(frame.mgmt.dst),
+                    'access_point_name': frame.ssid.data if hasattr(frame, 'ssid') else '(n/a)',
+                    'access_point_address': to_address(frame.mgmt.bssid)
+                }
+                addToArray(record["source_address"],record["strength"])
+                print record
+                
+            elif frame.type == dpkt.ieee80211.CTL_TYPE:
+                record = {
+                    'timestamp': '0',
+                    'type': 'control',
+                    'subtype': subtypes_control[frame.subtype],
+                    'strength': packet_signal,
+                    'source_address': '(n/a)', 
+                    'destination_address': '(n/a)', 
+                    'access_point_name': '(n/a)', 
+                    'access_point_address': '(n/a)' 
+                }
+                addToArray(record["source_address"],record["strength"])
+                print record
+                
+            elif frame.type == dpkt.ieee80211.DATA_TYPE:
+                record = {
+                    'timestamp': '0',
+                    'type': 'data',
+                    'subtype': subtypes_data[frame.subtype],
+                    'strength': packet_signal,
+                    'source_address': to_address(frame.data_frame.src),
+                    'destination_address': to_address(frame.data_frame.dst),
+                    'access_point_name': '(n/a)', 
+                    'access_point_address': to_address(frame.data_frame.bssid) if hasattr(frame.data_frame, 'bssid') else '(n/a)'
+                }
+                addToArray(record["source_address"],record["strength"])
+                print record
+            
+                 
+        except Exception as e:
+            print e            
+    packets.loop(-1, loop)
+
+def channels_switch(newchange):
+    channel = str(channels[0])
+    os.system(change_channel % channel)
+
+foudedMac=[]
+recordToExport=[]
+
+def addToArray(mac,rssi):
+    if mac not in foudedMac:
+        foudedMac.append(mac)
+        recordToExport.append([str(datetime.datetime.now()),mac,rssi])
+       
+    
+start()
---
+++ subtypes.py
@@ -0,0 +1,42 @@
+subtypes_management = {
+    0: 'association-request',
+    1: 'association-response',
+    2: 'reassociation-request',
+    3: 'reassociation-response',
+    4: 'probe-request',
+    5: 'probe-response',
+    8: 'beacon',
+    9: 'announcement-traffic-indication-message',
+    10: 'disassociation',
+    11: 'authentication',
+    12: 'deauthentication',
+    13: 'action'
+}
+
+subtypes_control = {
+    8: 'block-acknowledgement-request',
+    9: 'block-acknowledgement',
+    10: 'power-save-poll',
+    11: 'request-to-send',
+    12: 'clear-to-send',
+    13: 'acknowledgement',
+    14: 'contention-free-end',
+    15: 'contention-free-end-plus-acknowledgement'
+}
+
+subtypes_data = {
+    0: 'data',
+    1: 'data-and-contention-free-acknowledgement',
+    2: 'data-and-contention-free-poll',
+    3: 'data-and-contention-free-acknowledgement-plus-poll',
+    4: 'null',
+    5: 'contention-free-acknowledgement',
+    6: 'contention-free-poll',
+    7: 'contention-free-acknowledgement-plus-poll',
+    8: 'qos-data',
+    9: 'qos-data-plus-contention-free-acknowledgement',
+    10: 'qos-data-plus-contention-free-poll',
+    11: 'qos-data-plus-contention-free-acknowledgement-plus-poll',
+    12: 'qos-null',
+    14: 'qos-contention-free-poll-empty'
+}
